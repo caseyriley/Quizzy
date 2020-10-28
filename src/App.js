d719a13631499b1e5f4d4233f431be3c310b56a6
Casey <innerforest7@gmail.com>
2020-10-28 00:00:43 -0700

    make and implement all question and shuffle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import quizz from './Apprentice_TandemFor400_Data.json';
 import Question from './Question';
 
@@ -7,62 +7,37 @@ function App() {
 
 
 
-  function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
-  
-      // swap elements array[i] and array[j]
-      // we use "destructuring assignment" syntax to achieve that
-      // you'll find more details about that syntax in later chapters
-      // same can be written as:
-      // let t = array[i]; array[i] = array[j]; array[j] = t
-      [array[i], array[j]] = [array[j], array[i]];
+    function shuffle(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1)); 
+        [array[i], array[j]] = [array[j], array[i]];
+      }
     }
-  }
+    shuffle(quizz)
 
-  const [questionArrayState, setQuestionArrayState] = useState(shuffle(quizz))
-  console.log(questionArrayState)
+
+    function submit(){
+      
+    }
+    
+   
+ 
 
   return (
     <div id={"main"}>
       <h1 id={'main__title'}>QUIZZY</h1>
-      <div className={"question"}>
-        <h2 >{quizz[0].question}</h2>
-        <div className={"answer"}>
-          <input type="checkbox" name="check"/>
-          <p>{quizz[0].incorrect[0]}</p>
-        </div>
-        <div className={"answer"}>
-          <input type="checkbox" name="check"/>
-          <p>{quizz[0].incorrect[1]}</p>
-        </div>
-        <div className={"answer"}>
-          <input type="checkbox" name="check"/> 
-          <p>{quizz[0].incorrect[2]}</p>
-        </div>
-        <div className={"answer"}>
-          <input type="checkbox" name="check"/>
-          <p>{quizz[0].correct}</p>
-        </div>
-      </div>
-      <div className={"question"}>
-        <h2 >{quizz[1].question}</h2>
-        <div className={"answer"}>
-          <input type="checkbox" name="check"/>
-          <p>{quizz[1].incorrect[0]}</p>
-        </div>
-        <div className={"answer"}>
-          <input type="checkbox" name="check"/>
-          <p>{quizz[1].incorrect[1]}</p>
-        </div>
-        <div className={"answer"}>
-          <input type="checkbox" name="check"/> 
-          <p>{quizz[1].incorrect[2]}</p>
-        </div>
-        <div className={"answer"}>
-          <input type="checkbox" name="check"/>
-          <p>{quizz[1].correct}</p>
-        </div>
+      <Question question={quizz[0].question} answers={[quizz[0].incorrect[0], quizz[0].incorrect[1], quizz[0].incorrect[2], quizz[0].correct]} correct={quizz[0].correct}/>
+      <Question question={quizz[1].question} answers={[quizz[1].incorrect[0], quizz[1].incorrect[1], quizz[1].incorrect[2], quizz[1].correct]} correct={quizz[1].correct}/>
+      <Question question={quizz[2].question} answers={[quizz[2].incorrect[0], quizz[2].incorrect[1], quizz[2].incorrect[2], quizz[2].correct]} correct={quizz[2].correct}/>
+      <Question question={quizz[3].question} answers={[quizz[3].incorrect[0], quizz[3].incorrect[1], quizz[3].incorrect[2], quizz[3].correct]} correct={quizz[3].correct}/>
+      <Question question={quizz[4].question} answers={[quizz[4].incorrect[0], quizz[4].incorrect[1], quizz[4].incorrect[2], quizz[4].correct]} correct={quizz[4].correct}/>
+      <Question question={quizz[5].question} answers={[quizz[5].incorrect[0], quizz[5].incorrect[1], quizz[5].incorrect[2], quizz[5].correct]} correct={quizz[5].correct}/>
+      <Question question={quizz[6].question} answers={[quizz[6].incorrect[0], quizz[6].incorrect[1], quizz[6].incorrect[2], quizz[6].correct]} correct={quizz[6].correct}/>
+      <Question question={quizz[7].question} answers={[quizz[7].incorrect[0], quizz[7].incorrect[1], quizz[7].incorrect[2], quizz[7].correct]} correct={quizz[7].correct}/>
+      <Question question={quizz[8].question} answers={[quizz[8].incorrect[0], quizz[8].incorrect[1], quizz[8].incorrect[2], quizz[8].correct]} correct={quizz[8].correct}/>
+      <Question question={quizz[9].question} answers={[quizz[9].incorrect[0], quizz[9].incorrect[1], quizz[9].incorrect[2], quizz[9].correct]} correct={quizz[9].correct}/>
+      <div id={"submit"}> 
+        <h3>SUBMIT</h3>
       </div>
     </div>
 
--- src/Question.js
+++ src/Question.js
@@ -1,27 +1,68 @@
-import React from 'react';
-import quizz from './Apprentice_TandemFor400_Data.json';
+import React, {useState} from 'react';
 
-const Question = () => {
+
+const Question = (props) => {
+
+  const useEffect = (()=> {
+    function shuffle(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1)); 
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+    shuffle(props.answers)
+  },[])
+
+  
+
+  
+
+  // function onlyOne(checkbox) {
+  //   var checkboxes = document.getElementsByName('check')
+  //   checkboxes.forEach((item) => {
+  //     console.log("id=====>",item.id)
+  //       if (item.id !== checkbox) item.checked = false
+  //   })
+
+    const [checkState, setCheckState] = useState({0: false, 1: false, 2: false, 3: false});
+    function check0(){
+      setCheckState({0: true, 1: false, 2: false, 3: false})
+    }
+    function check1(){
+      setCheckState({0: false, 1: true, 2: false, 3: false})
+    }
+    function check2(){
+      setCheckState({0: false, 1: false, 2: true, 3: false})
+    }
+    function check3(){
+      setCheckState({0: false, 1: false, 2: false, 3: true})
+    }
+
+    // const useEffect = (()=>{
+    //   if (checkState[0] === true){
+    //     const checked = getElementById("checkbox-1")
+    //   }
+    // }, [checkState])
+
   return (
     <div className={"question"}>
-      
-        <h2 >{quizz[0].question}</h2>
-        <div className={"answer"}>
-          <input type="checkbox" name="check"/>
-          <p>{quizz[0].incorrect[0]}</p>
-        </div>
-        <div className={"answer"}>
-          <input type="checkbox" name="check"/>
-          <p>{quizz[0].incorrect[1]}</p>
-        </div>
-        <div className={"answer"}>
-          <input type="checkbox" name="check"/> 
-          <p>{quizz[0].incorrect[2]}</p>
-        </div>
-        <div className={"answer"}>
-          <input type="checkbox" name="check"/>
-          <p>{quizz[0].correct}</p>
-        </div>
+      <h2 >{props.question}</h2>
+      <div className={"answer"}>
+        <input className={"checkbox__input"} type="checkbox" name="check" onChange={check0} checked={checkState ? checkState[0] : false}/>
+        <p>{props.answers[0]}</p>
+      </div>
+      <div className={"answer"}>
+        <input className={"checkbox__input"} type="checkbox" name="check" onChange={check1} checked={checkState ? checkState[1] : false}/>
+        <p>{props.answers[1]}</p>
+      </div>
+      <div className={"answer"}>
+        <input className={"checkbox__input"} type="checkbox" name="check" onChange={check2} checked={checkState ? checkState[2] : false}/> 
+        <p>{props.answers[2]}</p>
+      </div>
+      <div className={"answer"}>
+        <input className={"checkbox__input"} type="checkbox" name="check" onChange={check3} checked={checkState ? checkState[3] : false}/>
+        <p>{props.answers[3]}</p>
+      </div>
     </div>
   )
 }
