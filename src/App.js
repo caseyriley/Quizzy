319865d36380974653e86bccca717d6b162a9fed
Casey <innerforest7@gmail.com>
2020-10-28 18:03:30 -0700

    make CSS BEM standards and add favicon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import quizz from './Apprentice_TandemFor400_Data.json';
 import Question from './Question';
 
+
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1)); 
@@ -33,60 +34,50 @@ shuffle(answersArray9)
 
 function App() {
 
-
-
-    
-
-    const [pointsState, setPointsState] = useState({"q0":0, "q1":0, "q2":0, "q3":0, "q4":0, "q5":0, "q6":0, "q7":0, "q8":0, "q9":0});
-    const [totallState, setTotallState] = useState(0);
-    const [scoreModalState, setScoreModalState] = useState(false)
-
+  const [pointsState, setPointsState] = useState({"q0":0, "q1":0, "q2":0, "q3":0, "q4":0, "q5":0, "q6":0, "q7":0, "q8":0, "q9":0});
+  const [totallState, setTotallState] = useState(0);
+  const [scoreModalState, setScoreModalState] = useState(false);
 
   function submit(){
     let count = 0;
     for (const answer in pointsState){
       count = count + pointsState[answer];
-      console.log(pointsState[answer]);
     }
     setTotallState(count);
     setScoreModalState(true);
-    console.log("totallState====>",totallState)
-  }
-    
-  function closeScoreModal(){
-    setScoreModalState(false);
   }
    
+  function reload() {
+    window.location.reload();
+  }
  
-
   return (
     <>
-      
-        <div id={"main"}>
-          <h1 id={'main__title'}>QUIZZY</h1>
-          <Question question={quizz[0].question} answers={answersArray0} correct={quizz[0].correct} pointsState={pointsState} setPointsState={setPointsState} qId={0}/>
-          <Question question={quizz[1].question} answers={answersArray1} correct={quizz[1].correct} pointsState={pointsState} setPointsState={setPointsState} qId={1}/>
-          <Question question={quizz[2].question} answers={answersArray2} correct={quizz[2].correct} pointsState={pointsState} setPointsState={setPointsState} qId={2}/>
-          <Question question={quizz[3].question} answers={answersArray3} correct={quizz[3].correct} pointsState={pointsState} setPointsState={setPointsState} qId={3}/>
-          <Question question={quizz[4].question} answers={answersArray4} correct={quizz[4].correct} pointsState={pointsState} setPointsState={setPointsState} qId={4}/>
-          <Question question={quizz[5].question} answers={answersArray5} correct={quizz[5].correct} pointsState={pointsState} setPointsState={setPointsState} qId={5}/>
-          <Question question={quizz[6].question} answers={answersArray6} correct={quizz[6].correct} pointsState={pointsState} setPointsState={setPointsState} qId={6}/>
-          <Question question={quizz[7].question} answers={answersArray7} correct={quizz[7].correct} pointsState={pointsState} setPointsState={setPointsState} qId={7}/>
-          <Question question={quizz[8].question} answers={answersArray8} correct={quizz[8].correct} pointsState={pointsState} setPointsState={setPointsState} qId={8}/>
-          <Question question={quizz[9].question} answers={answersArray9} correct={quizz[9].correct} pointsState={pointsState} setPointsState={setPointsState} qId={9}/>
-          <div id={"submit"}> 
-            <h3 onClick={submit}>SUBMIT</h3>
-          </div>
+      <div id={"main"}>
+        <h1 id={'main__title'}>QUIZZY</h1>
+        <Question question={quizz[0].question} answers={answersArray0} correct={quizz[0].correct} pointsState={pointsState} setPointsState={setPointsState} qId={0}/>
+        <Question question={quizz[1].question} answers={answersArray1} correct={quizz[1].correct} pointsState={pointsState} setPointsState={setPointsState} qId={1}/>
+        <Question question={quizz[2].question} answers={answersArray2} correct={quizz[2].correct} pointsState={pointsState} setPointsState={setPointsState} qId={2}/>
+        <Question question={quizz[3].question} answers={answersArray3} correct={quizz[3].correct} pointsState={pointsState} setPointsState={setPointsState} qId={3}/>
+        <Question question={quizz[4].question} answers={answersArray4} correct={quizz[4].correct} pointsState={pointsState} setPointsState={setPointsState} qId={4}/>
+        <Question question={quizz[5].question} answers={answersArray5} correct={quizz[5].correct} pointsState={pointsState} setPointsState={setPointsState} qId={5}/>
+        <Question question={quizz[6].question} answers={answersArray6} correct={quizz[6].correct} pointsState={pointsState} setPointsState={setPointsState} qId={6}/>
+        <Question question={quizz[7].question} answers={answersArray7} correct={quizz[7].correct} pointsState={pointsState} setPointsState={setPointsState} qId={7}/>
+        <Question question={quizz[8].question} answers={answersArray8} correct={quizz[8].correct} pointsState={pointsState} setPointsState={setPointsState} qId={8}/>
+        <Question question={quizz[9].question} answers={answersArray9} correct={quizz[9].correct} pointsState={pointsState} setPointsState={setPointsState} qId={9}/>
+        <div id={"main__submit"}> 
+          <h3 onClick={submit}>SUBMIT</h3>
         </div>
-      <div className={`${scoreModalState === true ? "score-modal-open" : "score-modal-close"}`} onClick={closeScoreModal}>
-        <div id={"score-modal-open__point-c"}>
-          <h1 id={"score-modal-open__point-c__title"}>YOUR SCORE</h1>
-          <h1 id={"score-modal-open__point-c__points"}>{totallState}</h1>
+      </div>
+      <div className={`${scoreModalState === true ? "main__score-modal-open" : "main__score-modal-close"}`} onClick={reload}>
+        <div id={"main__score-modal-open__point-c"}>
+          <h1 id={"main__score-modal-open__point-c__title"}>YOUR SCORE</h1>
+          <h1 id={"main__score-modal-open__point-c__points"}>{totallState}</h1>
         </div>
       </div>
     </>
 
   );
-}
+};
 
 export default App;
--- src/Question.js
+++ src/Question.js
@@ -1,111 +1,82 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 
 
 const Question = (props) => {
-  // let options = props.answers
-  // const useEffect = (()=> {
-  //   function shuffle(array) {
-  //     for (let i = array.length - 1; i > 0; i--) {
-  //       let j = Math.floor(Math.random() * (i + 1)); 
-  //       [array[i], array[j]] = [array[j], array[i]];
-  //     }
-  //   }
-  //   shuffle(options)
-  // },[])
 
-  
+  const [checkState, setCheckState] = useState({0: false, 1: false, 2: false, 3: false});
 
-  
+  function check0(){
+    setCheckState({0: true, 1: false, 2: false, 3: false})
+    if(props.answers[0] === props.correct) {
+      const points = props.pointsState;
+      points[`q${props.qId}`] = 1;
+      props.setPointsState(points);
+    } else {
+      const points = props.pointsState;
+      points[`q${props.qId}`] = 0;
+      props.setPointsState(points);
+    };
+  };
 
-  // function onlyOne(checkbox) {
-  //   var checkboxes = document.getElementsByName('check')
-  //   checkboxes.forEach((item) => {
-  //     console.log("id=====>",item.id)
-  //       if (item.id !== checkbox) item.checked = false
-  //   })
+  function check1(){
+    setCheckState({0: false, 1: true, 2: false, 3: false});
+    if(props.answers[1] === props.correct) {
+      const points = props.pointsState;
+      points[`q${props.qId}`] = 1;
+      props.setPointsState(points);
+    } else {
+      const points = props.pointsState;
+      points[`q${props.qId}`] = 0;
+      props.setPointsState(points);
+    };
+  };
 
-    const [checkState, setCheckState] = useState({0: false, 1: false, 2: false, 3: false});
-    function check0(){
-      setCheckState({0: true, 1: false, 2: false, 3: false})
-      if(props.answers[0] === props.correct) {
-        const points = props.pointsState;
-        points[`q${props.qId}`] = 1;
-        props.setPointsState(points)
-      } else {
-        const points = props.pointsState;
-        points[`q${props.qId}`] = 0;
-        props.setPointsState(points)
-      }
-      console.log("pointsState=====>", props.pointsState)
-      console.log(props.answers[0], props.correct)
-    }
-    function check1(){
-      setCheckState({0: false, 1: true, 2: false, 3: false})
-      if(props.answers[1] === props.correct) {
-        const points = props.pointsState;
-        points[`q${props.qId}`] = 1;
-        props.setPointsState(points)
-      } else {
-        const points = props.pointsState;
-        points[`q${props.qId}`] = 0;
-        props.setPointsState(points)
-      }
-    }
-    function check2(){
-      setCheckState({0: false, 1: false, 2: true, 3: false})
-      if(props.answers[2] === props.correct) {
-        const points = props.pointsState;
-        points[`q${props.qId}`] = 1;
-        props.setPointsState(points)
-      } else {
-        const points = props.pointsState;
-        points[`q${props.qId}`] = 0;
-        props.setPointsState(points)
-      }
-    }
-    function check3(){
-      setCheckState({0: false, 1: false, 2: false, 3: true})
-      if(props.answers[3] === props.correct) {
-        const points = props.pointsState;
-        points[`q${props.qId}`] = 1;
-        props.setPointsState(points)
-      } else {
-        const points = props.pointsState;
-        points[`q${props.qId}`] = 0;
-        props.setPointsState(points)
-      }
-    }
+  function check2(){
+    setCheckState({0: false, 1: false, 2: true, 3: false});
+    if(props.answers[2] === props.correct) {
+      const points = props.pointsState;
+      points[`q${props.qId}`] = 1;
+      props.setPointsState(points);
+    } else {
+      const points = props.pointsState;
+      points[`q${props.qId}`] = 0;
+      props.setPointsState(points);
+    };
+  };
+
+  function check3(){
+    setCheckState({0: false, 1: false, 2: false, 3: true});
+    if(props.answers[3] === props.correct) {
+      const points = props.pointsState;
+      points[`q${props.qId}`] = 1;
+      props.setPointsState(points);
+    } else {
+      const points = props.pointsState;
+      points[`q${props.qId}`] = 0;
+      props.setPointsState(points);
+    };
+  };
 
 
-    function shuffle(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1)); 
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-    }
-
-    // useEffect (() => {
-    //   shuffle(props.answers)
-    // }, [])
 
   return (
     <div className={"question"}>
       <h2 >{props.question}</h2>
       
-      <div className={"answer"}>
-        <input className={"checkbox__input"} type="checkbox" name="check" onChange={check0} checked={checkState ? checkState[0] : false}/>
+      <div className={"question__answer"}>
+        <input className={"question__answer__input"} type="checkbox" name="check" onChange={check0} checked={checkState ? checkState[0] : false}/>
         <p>{props.answers[0]}</p>
       </div>
-      <div className={"answer"}>
-        <input className={"checkbox__input"} type="checkbox" name="check" onChange={check1} checked={checkState ? checkState[1] : false}/>
+      <div className={"question__answer"}>
+        <input className={"question__answer__input"} type="checkbox" name="check" onChange={check1} checked={checkState ? checkState[1] : false}/>
         <p>{props.answers[1]}</p>
       </div>
-      <div className={"answer"}>
-        <input className={"checkbox__input"} type="checkbox" name="check" onChange={check2} checked={checkState ? checkState[2] : false}/> 
+      <div className={"question__answer"}>
+        <input className={"question__answer__input"} type="checkbox" name="check" onChange={check2} checked={checkState ? checkState[2] : false}/> 
         <p>{props.answers[2]}</p>
       </div>
-      <div className={"answer"}>
-        <input className={"checkbox__input"} type="checkbox" name="check" onChange={check3} checked={checkState ? checkState[3] : false}/>
+      <div className={"question__answer"}>
+        <input className={"question__answer__input"} type="checkbox" name="check" onChange={check3} checked={checkState ? checkState[3] : false}/>
         <p>{props.answers[3]}</p>
       </div>
     </div>
