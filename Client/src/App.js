21266a3b4ed6be09e726413b0d0e4ae94a812b28
Casey <innerforest7@gmail.com>
2021-02-14 22:03:19 -0800

    add favicon

--- Client/src/App.js
+++ Client/src/App.js
@@ -47,36 +47,12 @@ const App = () => {
 
       })
       .then(() => {
-        let answersArray0 =  [quizz[0]["qna"].incorrect[0], quizz[0]["qna"].incorrect[1], quizz[0]["qna"].incorrect[2], quizz[0]["qna"].correct];
-        shuffle(answersArray0)
-        updateQuestions("a0", answersArray0)
-        let answersArray1 =  [quizz[1]["qna"].incorrect[0], quizz[1]["qna"].incorrect[1], quizz[1]["qna"].incorrect[2], quizz[1]["qna"].correct];
-        shuffle(answersArray1)
-        updateQuestions("a1", answersArray1)
-        let answersArray2 =  [quizz[2]["qna"].incorrect[0], quizz[2]["qna"].incorrect[1], quizz[2]["qna"].incorrect[2], quizz[2]["qna"].correct];
-        shuffle(answersArray2)
-        updateQuestions("a2", answersArray2)
-        let answersArray3 =  [quizz[3]["qna"].incorrect[0], quizz[3]["qna"].incorrect[1], quizz[3]["qna"].incorrect[2], quizz[3]["qna"].correct];
-        shuffle(answersArray3)
-        updateQuestions("a3", answersArray3)
-        let answersArray4 =  [quizz[4]["qna"].incorrect[0], quizz[4]["qna"].incorrect[1], quizz[4]["qna"].incorrect[2], quizz[4]["qna"].correct];
-        shuffle(answersArray4)
-        updateQuestions("a4", answersArray4)
-        let answersArray5 =  [quizz[5]["qna"].incorrect[0], quizz[5]["qna"].incorrect[1], quizz[5]["qna"].incorrect[2], quizz[5]["qna"].correct];
-        shuffle(answersArray5)
-        updateQuestions("a5", answersArray5)
-        let answersArray6 =  [quizz[6]["qna"].incorrect[0], quizz[6]["qna"].incorrect[1], quizz[6]["qna"].incorrect[2], quizz[6]["qna"].correct];
-        shuffle(answersArray6)
-        updateQuestions("a6", answersArray6)
-        let answersArray7 =  [quizz[7]["qna"].incorrect[0], quizz[7]["qna"].incorrect[1], quizz[7]["qna"].incorrect[2], quizz[7]["qna"].correct];
-        shuffle(answersArray7)
-        updateQuestions("a7", answersArray7)
-        let answersArray8 =  [quizz[8]["qna"].incorrect[0], quizz[8]["qna"].incorrect[1], quizz[8]["qna"].incorrect[2], quizz[8]["qna"].correct];
-        shuffle(answersArray8)
-        updateQuestions("a8", answersArray8)
-        let answersArray9 =  [quizz[9]["qna"].incorrect[0], quizz[9]["qna"].incorrect[1], quizz[9]["qna"].incorrect[2], quizz[9]["qna"].correct];
-        shuffle(answersArray9)
-        updateQuestions("a9", answersArray9)
+        let numQ = 10;
+        for (let i = 0; i < numQ; i ++){
+          let answersArray = [quizz[i]["qna"].incorrect[0], quizz[i]["qna"].incorrect[1], quizz[i]["qna"].incorrect[2], quizz[i]["qna"].correct];
+          shuffle(answersArray)
+          updateQuestions(`a${i}`, answersArray)
+        }
         setQuestionsState(obj);
       })
       .catch(err => {
@@ -102,23 +78,6 @@ const App = () => {
   function reload() {
     window.location.reload();
   }
-  const qst = useMemo(()=>{
-    return(
-      quizzState && questionsState &&
-      <>
-        <Question question={quizzState[0]["qna"].question} answers={questionsState["a0"]} correct={quizzState[0] && quizzState[0]["qna"].correct} pointsState={pointsState} setPointsState={setPointsState} qId={0}/>
-        <Question question={quizzState[1]["qna"].question} answers={questionsState["a1"]} correct={quizzState[1] && quizzState[1]["qna"].correct} pointsState={pointsState} setPointsState={setPointsState} qId={1}/>
-        <Question question={quizzState[2]["qna"].question} answers={questionsState["a2"]} correct={quizzState[2] && quizzState[2]["qna"].correct} pointsState={pointsState} setPointsState={setPointsState} qId={2}/>
-        <Question question={quizzState[3]["qna"].question} answers={questionsState["a3"]} correct={quizzState[3] && quizzState[3]["qna"].correct} pointsState={pointsState} setPointsState={setPointsState} qId={3}/>
-        <Question question={quizzState[4]["qna"].question} answers={questionsState["a4"]} correct={quizzState[4] && quizzState[4]["qna"].correct} pointsState={pointsState} setPointsState={setPointsState} qId={4}/>
-        <Question question={quizzState[5]["qna"].question} answers={questionsState["a5"]} correct={quizzState[5] && quizzState[5]["qna"].correct} pointsState={pointsState} setPointsState={setPointsState} qId={5}/>
-        <Question question={quizzState[6]["qna"].question} answers={questionsState["a6"]} correct={quizzState[6] && quizzState[6]["qna"].correct} pointsState={pointsState} setPointsState={setPointsState} qId={6}/>
-        <Question question={quizzState[7]["qna"].question} answers={questionsState["a7"]} correct={quizzState[7] && quizzState[7]["qna"].correct} pointsState={pointsState} setPointsState={setPointsState} qId={7}/>
-        <Question question={quizzState[8]["qna"].question} answers={questionsState["a8"]} correct={quizzState[8] && quizzState[8]["qna"].correct} pointsState={pointsState} setPointsState={setPointsState} qId={8}/>
-        <Question question={quizzState[9]["qna"].question} answers={questionsState["a9"]} correct={quizzState[9] && quizzState[9]["qna"].correct} pointsState={pointsState} setPointsState={setPointsState} qId={9}/>
-      </>
-    )
-  },[quizzState, questionsState, pointsState])
  
   return (
     <>
@@ -131,12 +90,20 @@ const App = () => {
           <p>The correct answers will be displayed only after submitting your answers.</p>
           <p>The questions provided are random so each time you play the questions will be different.</p>
         </div>
-        {qst}
+
+        { quizzState && questionsState &&
+          [...Array(10)].map((el , i)=>{
+            return(
+            <Question key={i} question={quizzState[i]["qna"].question} answers={questionsState[`a${i}`]} correct={quizzState[i] && quizzState[i]["qna"].correct} pointsState={pointsState} setPointsState={setPointsState} qId={i}/>
+            )
+          })
+        }
+
         <div id={"main__submit"} onClick={submit}> 
           <h3 >SUBMIT</h3>
         </div>
       </div>
-      {quizzState ? 
+      
       <div className={`${scoreModalState === true ? "main__score-modal-open" : "main__score-modal-close"}`} onClick={reload} >
         <div id={"main__score-modal-open__point-c"}>
           <h1 id={"main__score-modal-open__point-c__title"}>YOUR SCORE</h1>
@@ -145,51 +112,20 @@ const App = () => {
             <div id={"answers-title"}>
               <h2 id={"answers-title__h2"}>correct answers below</h2>
             </div>
-            <div className={"modal-answer"}>
-              <p className={"modal-answer__p1"}>{quizzState[0]["qna"] ? quizzState[0]["qna"].question : ""}</p>
-              <p className={"modal-answer__p2"}>{quizzState[0]["qna"] ? quizzState[0]["qna"].correct : ""}</p>
-            </div>
-            <div className={"modal-answer"}>
-              <p className={"modal-answer__p1"}>{quizzState[1]["qna"].question}</p>
-              <p className={"modal-answer__p2"}>{quizzState[1]["qna"].correct}</p>
-            </div>
-            <div className={"modal-answer"}>
-              <p className={"modal-answer__p1"}>{quizzState[2]["qna"].question}</p>
-              <p className={"modal-answer__p2"}>{quizzState[2]["qna"].correct}</p>
-            </div>
-            <div className={"modal-answer"}>
-              <p className={"modal-answer__p1"}>{quizzState[3]["qna"].question}</p>
-              <p className={"modal-answer__p2"}>{quizzState[3]["qna"].correct}</p>
-            </div>
-            <div className={"modal-answer"}>
-              <p className={"modal-answer__p1"}>{quizzState[4]["qna"].question}</p>
-              <p className={"modal-answer__p2"}>{quizzState[4]["qna"].correct}</p>
-            </div>
-            <div className={"modal-answer"}>
-              <p className={"modal-answer__p1"}>{quizzState[5]["qna"].question}</p>
-              <p className={"modal-answer__p2"}>{quizzState[5]["qna"].correct}</p>
-            </div>
-            <div className={"modal-answer"}>
-              <p className={"modal-answer__p1"}>{quizzState[6]["qna"].question}</p>
-              <p className={"modal-answer__p2"}>{quizzState[6]["qna"].correct}</p>
-            </div>
-            <div className={"modal-answer"}>
-              <p className={"modal-answer__p1"}>{quizzState[7]["qna"].question}</p>
-              <p className={"modal-answer__p2"}>{quizzState[7]["qna"].correct}</p>
-            </div>
-            <div className={"modal-answer"}>
-              <p className={"modal-answer__p1"}>{quizzState[8]["qna"].question}</p>
-              <p className={"modal-answer__p2"}>{quizzState[8]["qna"].correct}</p>
-            </div>
-            <div className={"modal-answer"}>
-              <p className={"modal-answer__p1"}>{quizzState[9]["qna"].question}</p>
-              <p className={"modal-answer__p2"}>{quizzState[9]["qna"].correct}</p>
-            </div>
+            {quizzState && 
+              [...Array(10)].map((el,i)=>{
+                return(
+                  <div key={i} className={"modal-answer"}>
+                    <p className={"modal-answer__p1"}>{quizzState[i]["qna"].question}</p>
+                    <p className={"modal-answer__p2"}>{quizzState[i]["qna"].correct}</p>
+                  </div>
+                )
+              })
+            }
             <p></p>
           </div>
         </div>
       </div>
-      : null}
     </>
 
   );
