7539c18df79e07aaf03503addfadf094b19e4daa
Casey <innerforest7@gmail.com>
2021-02-16 18:08:46 -0800

    update score

--- Client/src/Components/ScoreModal.js
+++ Client/src/Components/ScoreModal.js
@@ -5,7 +5,7 @@ const ScoreModal = (props) => {
     <div className={`${props.scoreModalState === true ? "main__score-modal-open" : "main__score-modal-close"}`} onClick={props.reload} >
       <div id={"main__score-modal-open__point-c"}>
         <h1 id={"main__score-modal-open__point-c__title"}>YOUR SCORE</h1>
-        <h1 id={"main__score-modal-open__point-c__points"}>{props.totallState}</h1>
+        <h1 id={"main__score-modal-open__point-c__points"}>{props.totallState}/10</h1>
         <div className={"main__score-modal-open__answers"}>  
           <div id={"answers-title"}>
             <h2 id={"answers-title__h2"}>correct answers below</h2>
