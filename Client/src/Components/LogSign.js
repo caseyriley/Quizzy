1e88e9c85d5c27916a66a0afb3e7cd017864fb59
Casey <innerforest7@gmail.com>
2021-02-24 22:37:06 -0800

    create login form

--- Client/src/Components/LogSign.js
+++ Client/src/Components/LogSign.js
@@ -6,7 +6,45 @@ const LogSign = (props) => {
     <>
     { props.logSignState ?
       <div className={"login-form"} >
-
+        <label>
+          Email
+          <input
+          required
+          placeholder={"example@website.com"}
+          minLength={5} 
+          maxLength={150}
+          />
+        </label>
+        <label>
+          Password
+          <input
+          required
+          placeholder={"superSecret*"}
+          minLength={6} 
+          maxLength={150}
+          />
+        </label>
+        <label>
+          Name
+          <input
+          required
+          placeholder={"Robert Smith"}
+          minLength={3} 
+          maxLength={150}
+          />
+        </label>
+        <label>
+          Best coding Language
+          <input
+          required
+          placeholder={"javasnake"}
+          minLength={2} 
+          maxLength={150}
+          />
+        </label>
+        <div className={"log-sign-login-button"}>
+          <span>Submit</span>
+        </div>
       </div>
       :
       <div className={"signup-form"} >
