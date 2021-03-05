8709c8eec22c9fe260946197f4df5bf2a80fc874
Casey <innerforest7@gmail.com>
2021-03-04 22:24:56 -0800

    create demoLogin function

--- Client/src/Components/LoginForm.js
+++ Client/src/Components/LoginForm.js
@@ -32,6 +32,58 @@ const LoginForm = (props)=>{
     inner()
   }
 
+  async function demoLogin(){
+    const demoEmail = "quark@gmail.com";
+    const demoPassword = "QuantumFluctuation9";
+    let speed=70, i=1, k=0;
+
+    const ghostWriteEmail = async () => {
+        if (i <= demoEmail.length) {
+            let text = demoEmail.slice(0,i);
+            email.current.value = text;
+            i++;
+            setTimeout(ghostWriteEmail, speed);
+        }
+    }
+    const ghostWritePassword = async () => {
+        if (k <= demoPassword.length) {
+            let text = demoPassword.slice(0,k);
+            password.current.value = text;
+            k++;
+            setTimeout(ghostWritePassword, speed);
+        }
+    }
+    await ghostWriteEmail();
+    setTimeout(ghostWritePassword, speed*demoEmail.length);
+
+    
+
+    async function inner(){
+      const user = {email: email.current.value, password: password.current.value};
+      try {
+        const response = await fetch(`${API_URL}/users/login`, {
+          method: "POST",
+          mode: "cors",
+          body: JSON.stringify(user),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        const json = await response.json()
+        if (json["token"]){
+          console.log("JSON", json)
+          document.cookie = json["token"];
+          document.location.reload();
+        } else {
+          console.log("Error in login: 409")
+        }
+      } catch (error){
+        console.log("Error in login: ", error)
+      }
+    }
+    setTimeout(inner, 3300)
+  }
+
   return (
     <div className={"login-form"} >
       <label>
@@ -57,7 +109,7 @@ const LoginForm = (props)=>{
       <div className={"log-sign-login-button"} onClick={loginSubmit}>
         <span>Submit</span>
       </div>
-      <div className={"log-sign-demo-button"} >
+      <div className={"log-sign-demo-button"} onClick={demoLogin}>
         <span>Demo Login</span>
       </div>
     </div>
