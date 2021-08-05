4e30cda389ae83ba1b309a58e36ae04b10362f82
caseyriley <innerforest7@gmail.com>
2021-08-04 22:42:47 -0700

    clean up index.js

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-const express = require('express');
-const questionRouter = require('./routes/questions.js')
-const userRouter = require('./routes/users.js')
-const {sequelize} = require('./models')
-const env = process.env.NODE_ENV || 'development';
+const express = require("express");
+const questionRouter = require("./routes/questions.js");
+const userRouter = require("./routes/users.js");
+const { sequelize } = require("./models");
+const env = process.env.NODE_ENV || "development";
 
-const cors = require('cors')
-const path = require('path');
-const { request } = require('http');
+const cors = require("cors");
+const path = require("path");
+// const { request } = require("http");
 
 const app = express();
 
-app.use(cors(), express.json())
-app.use(express.static(path.join(__dirname, 'Client/build')));
-app.use('/questions', questionRouter);
-app.use('/users', userRouter);
+app.use(cors(), express.json());
+app.use(express.static(path.join(__dirname, "Client/build")));
+app.use("/questions", questionRouter);
+app.use("/users", userRouter);
 
+app.get("/test", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send("Route to /test working");
+});
 
-app.get('/test', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send("Route to /test working")
-})
-
-
-app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.sendFile(path.join(__dirname+'/Client/build/index.html'));
-})
-
-
-
+app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.sendFile(path.join(__dirname + "/Client/build/index.html"));
+});
 
 const port = process.env.PORT || 8080;
 
@@ -37,10 +32,8 @@ app.listen(port, async () => {
 
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
-})
-
-
+});
