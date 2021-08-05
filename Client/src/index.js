00dfb29fc02ebc25d7770972ce6d9f8e45a525a9
caseyriley <innerforest7@gmail.com>
2021-08-04 22:44:32 -0700

    clean up index.js

--- Client/src/index.js
+++ Client/src/index.js
@@ -17,22 +17,8 @@ import './dest/css/mScreenW300.css';
 import './dest/css/mScreenH750.css';
 import './dest/css/mScreenH650.css';
 
-
-
-
 import App from './App';
 import LoginPage from './Components/LoginPage';
-
-
-// const autoprefixer = require('autoprefixer')
-// const postcss = require('postcss')
-
-// postcss([autoprefixer]).process(css).then(result => {
-//   result.warnings().forEach(warn => {
-//     console.warn(warn.toString())
-//   })
-//   console.loc(result.css)
-// })
 
 
 let loggedIn = false;
