838b895399896a4b1856cd1b6a7f8b0cb3ed2e55
caseyriley <innerforest7@gmail.com>
2021-10-18 13:53:44 -0700

    convert CSS to SCSS

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './dest/css/app.css';
-import './dest/css/navbar.css';
-import './dest/css/addModal.css';
-import './dest/css/scoreModal.css';
-import './dest/css/loginPage.css';
+import './styles/app.css';
+import './styles/navbar.css';
+import './styles/addModal.css';
+import './styles/scoreModal.css';
+import './styles/loginPage.css';
 
 // import './styles/mScreenW700.css';
-import './dest/css/mScreenW750.css';
-import './dest/css/mScreenW650.css';
-import './dest/css/mScreenW500.css';
-import './dest/css/mScreenW400.css';
-import './dest/css/mScreenW350.css';
-import './dest/css/mScreenW300.css';
+import './styles/mScreenW750.css';
+import './styles/mScreenW650.css';
+import './styles/mScreenW500.css';
+import './styles/mScreenW400.css';
+import './styles/mScreenW350.css';
+import './styles/mScreenW300.css';
 
-import './dest/css/mScreenH750.css';
-import './dest/css/mScreenH650.css';
+import './styles/mScreenH750.css';
+import './styles/mScreenH650.css';
 
 import App from './App';
 import LoginPage from './Components/LoginPage';
